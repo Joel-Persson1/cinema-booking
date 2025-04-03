ecafe16b94cc7fd4ce76712f46e9baaf0ba93994
Joel-Persson1 <jolle.ov@gmail.com>
2025-04-03 09:52:16 +0200

    Added starter files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,36 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home, { loader as moviesLoader } from "./pages/Home";
+import Movie from "./pages/Movie";
+import AppLayout from "./pages/AppLayout";
+import Error from "./components/Error";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        loader: moviesLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: "/movie/:id",
+        element: <Movie />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/booking",
+        element: <Cart />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
