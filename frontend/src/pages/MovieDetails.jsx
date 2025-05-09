db7d59bead395706127c50fc32b749befb3d77c0
Joel-Persson1 <jolle.ov@gmail.com>
2025-05-09 20:20:50 +0200

    fixed styling on choose date on movie

--- frontend/src/pages/MovieDetails.jsx
+++ frontend/src/pages/MovieDetails.jsx
@@ -1,57 +1,3 @@
-// import ReactPlayer from "react-player";
-// import { useLoaderData } from "react-router-dom";
-// import { getMovieById, getScheduleById } from "../services/MovieApi";
-// import ScheduleById from "../components/ScheduleById";
-// import "../styles/MovieDetails.css";
-
-// export default function MovieDetails() {
-//   const { movie, schedule } = useLoaderData();
-
-//   console.log(schedule);
-
-//   if (movie.error) return <p>Movie not found</p>;
-
-//   return (
-//     <div className="movie-details-container">
-//       <div className="movie-content">
-//         <div className="trailer-container">
-//           <ReactPlayer url={movie.trailer_url} width="100%" controls={true} />
-//         </div>
-
-//         <div className="poster-container">
-//           <img src={movie.poster_url} alt={`${movie.title} poster`} />
-//         </div>
-//       </div>
-
-//       <div className="movie-info-container">
-//         <h1 className="movie-title">{movie.title}</h1>
-
-//         <div className="movie-meta">
-//           <p>{movie.year}</p>
-//           <p>{movie.runtime}</p>
-//           <p>{movie.genre}</p>
-//           <p>{movie.director}</p>
-//         </div>
-
-//         <p className="movie-plot">{movie.plot}</p>
-//       </div>
-
-//       <ScheduleById schedule={schedule} />
-//     </div>
-//   );
-// }
-
-// export async function loader({ params }) {
-//   const movie = await getMovieById(params.movieId);
-//   const schedule = await getScheduleById(params.movieId);
-
-//   const uniqueDates = [...new Set(schedule.map((s) => s.date))];
-
-//   console.log(uniqueDates);
-
-//   return { movie, schedule };
-// }
-
 import React, { useState, useEffect, useMemo } from "react";
 import ReactPlayer from "react-player";
 import { useLoaderData } from "react-router-dom";
@@ -124,7 +70,11 @@ export default function MovieDetails() {
             <button
               key={date}
               onClick={() => setSelectedDate(date)}
-              className={selectedDate === date ? "selected-date" : ""}
+              className={
+                selectedDate === date
+                  ? "screeningButton selected-date"
+                  : "screeningButton"
+              }
             >
               {new Date(date).toLocaleDateString("sv-SE", {
                 weekday: "short",
